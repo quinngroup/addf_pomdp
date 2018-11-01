f45ab384a515edece2640698ce64c1ee75a7eaf4
Roi Ceren <roi.ceren@salesloft.com>
2018-11-01 15:36:42 -0400

    Finished and tested observation and transition functions, can now fully simulate a growing season

--- crop_simulate.py
+++ crop_simulate.py
@@ -4,7 +4,8 @@ from numpy.linalg import norm
 import sys
 import glob
 import re
-from random import randint as rand
+from random import randint
+from random import random
 
 # Image packages
 import cv2
@@ -15,12 +16,21 @@ Crop field simulator, containing the true observation function, which is unobser
 """
 
 class CropField:
-    def __init__(self, n_observation, image_path = ''):
-        # Create 2 to 4 sectors with whether there is a stress or not
-        self.sectors = np.array(np.random.rand((rand(2,4))).round(), dtype=np.int)
+    """
+    -------------------------------------------------------------------------------------------------
+    Initialization
+    -------------------------------------------------------------------------------------------------
+    """
+    def __init__(self, n_sector, n_observation, image_path = ''):
+        # Build frame
         self.create_observation_function(n_observation)
         self.create_transition_function()
-        print(self.sectors)
+
+        # Create random initial state for simulated sectors
+        self.cur_sectors = np.array(np.random.rand((n_sector)).round(), dtype=np.int)
+
+        self.day = 0
+
         # TODO: Process an image to generate simulation for bootstrapping
         # image = cv2.imread(image_path,0)
         # self.dimensions = image.shape[:2]
@@ -29,12 +39,12 @@ class CropField:
         """
         Instantiate the observation function.
 
-        The level of the agent makes stresses harder to distinguish.
+        The layer of the agent makes stresses harder to distinguish.
         """
-        self.o = np.array([self.create_observation_function_level(n_observation, i) for i in range(1,3)])
+        self.o = np.array([self.create_observation_function_layer(n_observation, i) for i in range(1,4)])
 
-    def create_observation_function_level(self, n_observation, level):
-        true_likelihood = 0.85 - level*0.05 # The likelihood of truly positively identifying the stress
+    def create_observation_function_layer(self, n_observation, layer):
+        true_likelihood = 0.85 - layer*0.05 # The likelihood of truly positively identifying the stress
         stress = [(i/n_observation)*(1-true_likelihood) for i in range(1,n_observation)] + [true_likelihood]
         return(np.array([
                 list(reversed(stress)), # No stress
@@ -48,12 +58,58 @@ class CropField:
         Actions don't affect the state transition. States are less likely to transition later in the growing season.
         """
         t = []
-        persist = np.array([0.7, 0.3]) # Initial likelihood the state persists
+        persist = np.array([0.5, 0.5]) # Initial likelihood the state persists
         for i in range(1,24):
             modified = np.array([persist[0]*i,persist[1]])
             modified = modified/modified.sum()
             t.append([modified, list(reversed(modified))])
         self.t = np.array(t)
 
-    def observe():
-        print("stub")
+    """
+    -------------------------------------------------------------------------------------------------
+    Observing and transitioning between sets of sectors
+    -------------------------------------------------------------------------------------------------
+    """
+    def observe_sectors(self,layer):
+        return([self.observe(s,layer) for s in self.cur_sectors])
+
+    def observe(self,s,l):
+        ob = random()
+        for i in range(0,len(self.o[l][s])):
+            ob -= self.o[l][s][i]
+            if ob <= 0:
+                return i
+        return len(self.o[l][s])-1
+
+    def transition_sectors(self):
+        return([self.transition(s) for s in self.cur_sectors])
+
+    def transition(self,s):
+        tr = random()
+        d = min([self.day,23])
+        for i in range(0,len(self.t[d][s])):
+            tr -= self.t[d][s][i]
+            if tr <= 0:
+                return i
+        return len(self.t[s])-1
+
+    def iterate_states(self):
+        self.cur_sectors = self.transition_sectors()
+        self.day += 1
+        # new_sectors = self.transition_sectors()
+        # self.cur_sectors = new_sectors
+
+    """
+    -------------------------------------------------------------------------------------------------
+    Testing the CropField simulator
+    -------------------------------------------------------------------------------------------------
+    """
+
+    def test(self):
+        cur = self.cur_sectors
+        print("Current State: "+str(cur))
+        for i in reversed(range(1,4)):
+            print("\tLayer "+str(i)+" observation")
+            print("\t"+str(self.observe_sectors(i-1)))
+        self.iterate_states()
+        print("\tIterated from "+str(cur)+" to "+str(self.cur_sectors))
